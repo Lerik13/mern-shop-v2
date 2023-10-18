ccfa5b79aeb978e3f8214e2edcc382d222d35f6f
Valeria <lerik13@gmail.com>
2023-10-17 23:46:28 -0600

    + components Haeader & Footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 const App = () => {
 	return (
-		<div>Welcome to e-Shop</div>
+		<>
+			<Header />
+			<main className="py-3">
+				<Container>
+					<h1>Welcome to e-Shop</h1>
+				</Container>
+			</main>
+			<Footer />
+		</>
 	)
 }
 
