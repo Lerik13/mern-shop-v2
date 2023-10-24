5a0dbd3fc26d2df9ea0d37c84bbc9c314e8bfbe8
Valeria <lerik13@gmail.com>
2023-10-24 10:05:54 -0600

    frontend: Redux Store & API Slice

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {
@@ -6,6 +7,8 @@ import {
 	Route,
 	RouterProvider
 } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 //import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/styles/bootstrap.custom.css';
 import './assets/styles/index.css';
@@ -25,9 +28,11 @@ const router = createBrowserRouter(
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+	<React.StrictMode>
+		<Provider store={store}>
+			<RouterProvider router={router} />
+		</Provider>
+	</React.StrictMode>
 );
 
 reportWebVitals();
